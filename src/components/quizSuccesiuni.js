c1d1b58b900866b1475e6e3bc0d22d5b790d87fe
Florin-Catalin <florin.g.ungureanu@gmail.com>
2025-06-26 03:22:59 +0300

    finished adding all questions

--- src/components/quizSuccesiuni.js
+++ src/components/quizSuccesiuni.js
@@ -1,17 +1,17 @@
 export const quizSuccesiuni = {
   quizTitle: "DREPT CIVIL SUCCESIUNI",
   quizSynopsis: "Întrebări din drept civil - succesiuni.",
-  nrOfQuestions: 4,
+  nrOfQuestions: 100,
   questions: [
     {
-      id: 301,
-      question: "În caz de deces, transmisiunea patrimoniului defunctului se realizează:",
+      id: 401,
+      question: "401. În caz de deces, transmisiunea patrimoniului defunctului se realizează:",
       questionType: "text",
       answerSelectionType: "single",
       answers: [
-        "a) exclusiv cu respectarea regulilor moştenirii legale, în toate cazurile în care defunctul nu a întocmit testament;",
-        "b) exclusiv cu respectarea regulilor moştenirii testamentare, în toate cazurile în care defunctul a întocmit un testament;",
-        "c) exclusiv cu respectarea regulilor moştenirii testamentare, dacă defunctul a încălcat rezerva succesorală a moştenitorilor rezervatari."
+        "a) exclusiv cu respectarea regulilor moștenirii legale, în toate cazurile în care defunctul nu a întocmit testament;",
+        "b) exclusiv cu respectarea regulilor moștenirii testamentare, în toate cazurile în care defunctul a întocmit un testament;",
+        "c) exclusiv cu respectarea regulilor moștenirii testamentare, dacă defunctul a încălcat rezerva succesorală a moștenitorilor rezervatari."
       ],
       correctAnswer: "2",
       messageForCorrectAnswer: "Corect! Dacă există testament, se aplică regulile moștenirii testamentare.",
@@ -19,14 +19,14 @@ export const quizSuccesiuni = {
       point: 1
     },
     {
-      id: 302,
-      question: "Moştenitorii testamentari se deosebesc de moştenitorii legali care nu vin la succesiune doar pentru a culege rezerva succesorală:",
+      id: 402,
+      question: "402. Moștenitorii testamentari se deosebesc de moștenitorii legali care nu vin la succesiune doar pentru a culege rezerva succesorală:",
       questionType: "text",
       answerSelectionType: "single",
       answers: [
-        "a) moştenitorii testamentari pot fi moştenitori cu titlu particular;",
-        "b) moştenitorii testamentari pot fi moştenitori cu titlu universal;",
-        "c) legatarii universali nu au vocație la întreaga moştenire lăsată de defunct în cazul în care moştenirea este acceptată de cel puțin doi dintre aceștia."
+        "a) moștenitorii testamentari pot fi moștenitori cu titlu particular;",
+        "b) moștenitorii testamentari pot fi moștenitori cu titlu universal;",
+        "c) legatarii universali au vocație la întreaga moștenire lăsată de defunct în cazul în care moștenirea este acceptată de cel puțin doi dintre aceștia."
       ],
       correctAnswer: "2",
       messageForCorrectAnswer: "Corect! Moștenitorii testamentari pot fi cu titlu universal.",
@@ -34,13 +34,13 @@ export const quizSuccesiuni = {
       point: 1
     },
     {
-      id: 303,
-      question: "În ipoteza în care defunctul lasă testament:",
+      id: 403,
+      question: "403. În ipoteza în care defunctul lasă testament:",
       questionType: "text",
       answerSelectionType: "single",
       answers: [
-        "a) moştenitorul testamentar nu poate dobândi dreptul de proprietate asupra unui imobil, decât în condițiile în care testamentul este întocmit in formă autentică;",
-        "b) defunctul poate transmite o fracţiune a patrimoniului unei persoane fizice sau juridice în fiinţă;",
+        "a) moștenitorul testamentar nu poate dobândi dreptul de proprietate asupra unui imobil, decât în condițiile în care testamentul este întocmit în formă autentică;",
+        "b) defunctul poate transmite o fracțiune a patrimoniului unei persoane fizice sau juridice în ființă;",
         "c) indiferent de forma testamentului, defunctul nu poate transmite întreg patrimoniul, în nicio situație."
       ],
       correctAnswer: "2",
@@ -49,18 +49,213 @@ export const quizSuccesiuni = {
       point: 1
     },
     {
-      id: 304,
-      question: "În materia succesiunii:",
+      id: 404,
+      question: "404. În materia succesiunii:",
       questionType: "text",
       answerSelectionType: "single",
       answers: [
         "a) succesiunea se deschide în toate cazurile la decesul persoanei fizice;",
-        "b) persoana fizică sau juridică nu poate culege o parte a succesiunii în calitate de moştenitor legatar, iar altă parte în calitate de moştenitor legal;",
-        "c) persoana fizică sau juridică nu poate accepta succesiunea în calitate de moştenitor testamentar, dar să refuze acceptarea acesteia în calitate de moştenitor legal."
+        "b) persoana fizică sau juridică nu poate culege o parte a succesiunii în calitate de moștenitor legatar, iar altă parte în calitate de moștenitor legal;",
+        "c) persoana fizică sau juridică nu poate accepta succesiunea în calitate de moștenitor testamentar, dar să refuze acceptarea acesteia în calitate de moștenitor legal."
       ],
       correctAnswer: "1",
       messageForCorrectAnswer: "Corect! Succesiunea se deschide la decesul persoanei fizice.",
       messageForIncorrectAnswer: "Greșit. Răspunsul corect este a).",
+      point: 1
+    },
+    {
+      id: 405,
+      question: "405. Ca efect al indivizibilității transmisiunii:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) un succesibil legal nu poate accepta doar o parte din bunuri, iar la cealaltă să renunțe;",
+        "b) toate drepturile și obligațiile patrimoniale ce au aparținut defunctului se transmit tuturor succesibililor;",
+        "c) se pot aplica norme diferite în funcție de categoria de bunuri, mobile sau imobile."
+      ],
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Corect! Nu se poate accepta doar o parte din bunuri.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este a).",
+      point: 1
+    },
+    {
+      id: 406,
+      question: "406. Succesibilul este persoana care:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) a acceptat succesiunea;",
+        "b) a renunțat la moștenire;",
+        "c) are vocație succesorală, dar încă nu și-a manifestat dreptul de opțiune succesorală."
+      ],
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Corect! Succesibilul are vocație succesorală, dar nu și-a manifestat încă dreptul de opțiune.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este c).",
+      point: 1
+    },
+    {
+      id: 407,
+      question: "407. Succesibilul este persoana care:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) are vocație succesorală dar încă nu și-a manifestat dreptul de opțiune succesorală;",
+        "b) are vocație succesorală dar a rămas străin de succesiune;",
+        "c) are vocație succesorală dar a renunțat la succesiune."
+      ],
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Corect! Succesibilul are vocație succesorală dar nu și-a manifestat dreptul de opțiune.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este a).",
+      point: 1
+    },
+    {
+      id: 408,
+      question: "408. Locul deschiderii succesiunii este:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) locul decesului defunctului;",
+        "b) locul unde se află bunurile imobile;",
+        "c) ultimul domiciliu al defunctului."
+      ],
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Corect! Locul deschiderii succesiunii este ultimul domiciliu al defunctului.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este c).",
+      point: 1
+    },
+    {
+      id: 409,
+      question: "409. Locul deschiderii succesiunii este:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) ultimul domiciliul al defunctului inserat în cartea de identitate;",
+        "b) ultima reședință a defunctului;",
+        "c) ultimul domiciliul real al defunctului."
+      ],
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Corect! Ultimul domiciliu inserat în cartea de identitate.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este a).",
+      point: 1
+    },
+    {
+      id: 410,
+      question: "410. Persoanele care au vocație succesorală legală sunt:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) moștenitorii testamentari;",
+        "b) rudele defunctului, indiferent de gradul de rudenie;",
+        "c) descendenții defunctului."
+      ],
+      correctAnswer: "2",
+      messageForCorrectAnswer: "Corect! Rudele defunctului, indiferent de grad.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este b).",
+      point: 1
+    },
+    {
+      id: 411,
+      question: "411. Normele juridice care reglementează succesiunea legală:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) sunt imperative;",
+        "b) sunt dispozitive;",
+        "c) pot fi modificate cu acordul moștenitorilor."
+      ],
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Corect! Sunt imperative.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este a).",
+      point: 1
+    },
+    {
+      id: 412,
+      question: "412. Capacitatea succesorală:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) este recunoscută persoanelor juridice întotdeauna, de la data actului de înființare, cu condiția respectării principiului specialității capacității de folosință;",
+        "b) se desființează cu efect retroactiv în cazul constatării decesului persoanei dispărute ca fiind anterioară morții celui care lasă moștenirea;",
+        "c) este recunoscută copilului conceput, cu condiția ca să fie viabil."
+      ],
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Corect! Copilul conceput are capacitate succesorală dacă este viabil.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este c).",
+      point: 1
+    },
+    {
+      id: 413,
+      question: "413. Nu au capacitate succesorală de a dobândi bunuri prin moștenire legală:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) persoanele fizice predecedate, cu excepția celor care nu pot fi reprezentate;",
+        "b) comorienții;",
+        "c) persoanele nedemne de a moșteni."
+      ],
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Corect! Persoanele nedemne nu au capacitate succesorală.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este c).",
+      point: 1
+    },
+    {
+      id: 414,
+      question: "414. Cetățenii străini sau apatrizii pot dobândi dreptul de proprietate asupra terenurilor:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) prin moștenire legală sau testamentară;",
+        "b) prin moștenire legală, indiferent că aparțin sau nu unor state membre ale Uniunii Europene;",
+        "c) prin moștenire legală, dar numai dacă aparțin unor state membre ale Uniunii Europene."
+      ],
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Corect! Numai dacă aparțin unor state membre UE.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este c).",
+      point: 1
+    },
+    {
+      id: 415,
+      question: "415. Actele juridice referitoare la bunurile moștenirii, încheiate de un nedemn față de moștenitorii legali sau testamentari:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) în măsura în care sunt menținute ca valabile, obligă nedemnul la plata de despăgubiri către adevărații moștenitori și ca un posesor de rea credință;",
+        "b) sunt desființate toate cu efect retroactiv;",
+        "c) sunt menținute ca valabile numai dacă sunt acte de conservare și de administrare."
+      ],
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Corect! Nedemnul răspunde ca un posesor de rea credință.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este a).",
+      point: 1
+    },
+    {
+      id: 416,
+      question: "416. Nedemnitatea succesorală:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) poate fi invocată doar de comostenitori legali sau, după caz, de moștenitorii subsecvenți;",
+        "b) produce efecte de la data deschiderii moștenirii;",
+        "c) poate fi constatată de instanță și anterior deschiderii succesiunii."
+      ],
+      correctAnswer: "2",
+      messageForCorrectAnswer: "Corect! Produce efecte de la data deschiderii moștenirii.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este b).",
+      point: 1
+    },
+    {
+      id: 417,
+      question: "417. Nedemnul care a intrat deja în posesia unor bunuri din masa succesorală, înainte de deschiderea moștenirii:",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
+        "a) este privit ca un posesor de rea-credință de la data constatării nedemnității;",
+        "b) trebuie să restituie celor îndreptățiți contravaloarea acestora, chiar dacă s-ar putea restitui în natură;",
+        "c) este privit ca un posesor de rea-credință de la data deschiderii succesiunii."
+      ],
+      correctAnswer: "3",
+      messageForCorrectAnswer: "Corect! De la data deschiderii succesiunii.",
+      messageForIncorrectAnswer: "Greșit. Răspunsul corect este c).",
       point: 1
     }
   ]
